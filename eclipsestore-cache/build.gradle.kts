afe43583e0309da3b707d475c8fbb01dea09cf68
Sergio del Amo <sergio.delamo@softamo.com>
2024-02-09 16:56:15 +0100

    build: enable binary compatibility

--- eclipsestore-cache/build.gradle.kts
+++ eclipsestore-cache/build.gradle.kts
@@ -2,13 +2,6 @@ plugins {
     id("io.micronaut.internal.build.eclipsestore-module")
 }
 
-// REMOVE ONCE FIRST RELEASE IS RELEASED
-micronautBuild {
-    binaryCompatibility {
-        enabled.set(false)
-    }
-}
-
 dependencies {
     api(projects.micronautEclipsestore)
     api(libs.managed.eclipsestore.cache)
