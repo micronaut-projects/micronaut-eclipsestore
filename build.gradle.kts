f66c282c80ff8c7944dfd0700e8469c53e36a6da
Tim Yates <tim.yates@gmail.com>
2024-02-27 15:59:55 +0000

    Convert base build to kotlin and move preferProjectModules

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("io.micronaut.build.internal.docs")
+    id("io.micronaut.build.internal.quality-reporting")
+    id("io.micronaut.internal.build.eclipsestore-rest-cli")
+}
+
+repositories {
+    mavenCentral()
+}
