fa2d4cdf858fc1e9186de51fd27567720edb728e
Tim Yates <tim.yates@gmail.com>
2024-02-27 15:57:44 +0000

    Add eclipsestore-processor to settings and convert to kotlin

---
+++ settings.gradle.kts
@@ -0,0 +1,40 @@
+import io.micronaut.build.MicronautBuildSettingsExtension
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("io.micronaut.build.shared.settings") version "6.6.2"
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+rootProject.name = "eclipsestore-parent"
+
+include("eclipsestore-bom")
+include("eclipsestore")
+include("eclipsestore-annotations")
+include("eclipsestore-cache")
+include("eclipsestore-rest")
+include("eclipsestore-processor")
+
+include("test-suite")
+include("test-suite-utils")
+include("test-suite-groovy")
+include("test-suite-kotlin")
+
+configure<MicronautBuildSettingsExtension> {
+    useStandardizedProjectNames.set(true)
+    importMicronautCatalog()
+    importMicronautCatalog("micronaut-aws")
+    importMicronautCatalog("micronaut-cache")
+    importMicronautCatalog("micronaut-micrometer")
+    importMicronautCatalog("micronaut-serde")
+    importMicronautCatalog("micronaut-sql")
+    importMicronautCatalog("micronaut-test-resources")
+    importMicronautCatalog("micronaut-validation")
+}
